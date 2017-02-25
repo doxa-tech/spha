d50f47c541a370b64225763a5c9d1e65e16f70f6
Noémien Kocher <nkcr.je@gmail.com>
2017-02-25 16:08:59 +0100

    fix controller

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -4,8 +4,8 @@ class DeploysController < ApplicationController
   def deploy
     @site = params[:site]
     @res = case @site
-      when "fribourg" then Open3.capture2e("bash #{Rails.root('lib', 'scripts')}/deploy_fribourg.sh")
-      when "romont"   then Open3.capture2e("bash #{Rails.root('lib', 'scripts')}/deploy_romont.sh")
+      when "fribourg" then Open3.capture2e("bash #{Rails.root.join('lib', 'scripts').to_s}/deploy_fribourg.sh")
+      when "romont"   then Open3.capture2e("bash #{Rails.root.join('lib', 'scripts').to_s}/deploy_romont.sh")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -2,8 +2,8 @@ class PreviewsController < ApplicationController
   def preview
     @site = params[:site]
     @res = case @site
-      when "fribourg" then Open3.capture2e("bash #{Rails.root('lib', 'scripts')}/preview_fribourg.sh")
-      when "romont"   then Open3.capture2e("bash #{Rails.root('lib', 'scripts')}/preview_romont.sh")
+      when "fribourg" then Open3.capture2e("bash #{Rails.root.join('lib', 'scripts').to_s}/preview_fribourg.sh")
+      when "romont"   then Open3.capture2e("bash #{Rails.root.join('lib', 'scripts').to_s}/preview_romont.sh")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
