2a25ac55af6ddd5ae194c84f6ba59c912c5e79bf
Noémien Kocher <nkcr.je@gmail.com>
2017-02-25 23:18:19 +0100

    update script vars

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -3,10 +3,9 @@ class DeploysController < ApplicationController
 
   def deploy
     @site = params[:site]
-    p "bash #{Rails.root.join('lib', 'scripts').to_s}/deploy_fribourg.sh"
     @res = case @site
-    when "fribourg" then Open3.capture2e("bash #{ENV['SCRIPT_PATH']}/deploy_fribourg.sh")
-      when "romont"   then Open3.capture2e("bash #{Rails.root.join('lib', 'scripts').to_s}/deploy_romont.sh")
+    when "fribourg" then Open3.capture2e("bash lib/scripts/deploy_fribourg.sh")
+    when "romont"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
--- lib/scripts/deploy_fribourg.sh
+++ lib/scripts/deploy_fribourg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u spha ssh $FRIBOURG_USER@146.185.163.64 -p $JSPORT -o ConnectTimeout=10 -o PasswordAuthentication=false -o BatchMode=yes "cd /var/www/fribourg.eebulle.ch/public_html/eebulle.fribourg/ && git pull"
+sudo -u spha ssh $SPHA_FRIBOURG_USER@146.185.163.64 -p $SPHA_JSPORT -o ConnectTimeout=10 -o PasswordAuthentication=false -o BatchMode=yes "cd /var/www/fribourg.eebulle.ch/public_html/eebulle.fribourg/ && git pull"
