bbd81c809b19f41e58ce5705bfacef08d9b28e07
Noémien Kocher <nkcr.je@gmail.com>
2017-02-26 00:06:20 +0100

    updated other scripts

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -3,7 +3,7 @@ class PreviewsController < ApplicationController
     @site = params[:site]
     @res = case @site
     when "fribourg" then Open3.capture2e("bash lib/scripts/preview_fribourg.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
-    when "romont"   then Open3.capture2e("bash lib/scripts/preview_romont.sh")
+    when "romont"   then Open3.capture2e("bash lib/scripts/preview_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
