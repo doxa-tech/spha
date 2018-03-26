47ea0473e475c0aadb628e504920e6885020be26
Noémien Kocher <nkcr.je@gmail.com>
2018-03-27 00:29:57 +0200

    revert

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -4,8 +4,13 @@ class DeploysController < ApplicationController
   def deploy
     @site = params[:site]
     @res = case @site
+<<<<<<< HEAD
     when "quartierlibreromont_ch" then Open3.capture2e("bash lib/scripts/deploy_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
     when "romont_eebulle_ch"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
+=======
+    when "quartierlibreromont" then Open3.capture2e("bash lib/scripts/deploy_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
+    when "romont"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
+>>>>>>> parent of 0121c1e... refactoring
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
