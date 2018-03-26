3dd9e2801c298266591c4e11ed1d47e927eb9c8f
Noémien Kocher <nkcr.je@gmail.com>
2018-03-27 00:37:47 +0200

    fix

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -1,15 +1,16 @@
 class PreviewsController < ApplicationController
   def preview
     @site = params[:site]
+    @site_id = @site
     @res = case @site
-<<<<<<< HEAD
-    when "quartierlibreromont_ch" then Open3.capture2e("bash lib/scripts/preview_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
-    when "romont_eebulle_ch"   then Open3.capture2e("bash lib/scripts/preview_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
-=======
-    when "quartierlibreromont" then Open3.capture2e("bash lib/scripts/preview_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
-    when "romont"   then Open3.capture2e("bash lib/scripts/preview_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
->>>>>>> parent of 0121c1e... refactoring
-      else ["Unknown site", -1]
+    when "quartierlibreromont" then 
+      @site = "preview.quartierlibreromont.ch"
+      Open3.capture2e("bash lib/scripts/preview_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
+    when "romont" then
+      @site = "preview-eebulle.romont.ch"
+      Open3.capture2e("bash lib/scripts/preview_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
+    else 
+      ["Unknown site: #{@site}", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
     @open_preview = params[:open_preview]
