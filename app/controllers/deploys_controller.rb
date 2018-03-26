5fccfdea25c46d682214f4617fb16291d806f193
Noémien Kocher <nkcr.je@gmail.com>
2018-03-27 00:19:12 +0200

    name fix

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -4,8 +4,8 @@ class DeploysController < ApplicationController
   def deploy
     @site = params[:site]
     @res = case @site
-    when "quartierlibreromont.ch" then Open3.capture2e("bash lib/scripts/deploy_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
-    when "romont.eebulle.ch"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
+    when "quartierlibreromont_ch" then Open3.capture2e("bash lib/scripts/deploy_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
+    when "romont_eebulle_ch"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -2,8 +2,8 @@ class PreviewsController < ApplicationController
   def preview
     @site = params[:site]
     @res = case @site
-    when "quartierlibreromont.ch" then Open3.capture2e("bash lib/scripts/preview_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
-    when "romont.eebulle.ch"   then Open3.capture2e("bash lib/scripts/preview_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
+    when "quartierlibreromont_ch" then Open3.capture2e("bash lib/scripts/preview_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
+    when "romont_eebulle_ch"   then Open3.capture2e("bash lib/scripts/preview_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
