0121c1e07d635959231949fdabd03cb63ee12af1
Noémien Kocher <nkcr.je@gmail.com>
2018-03-26 13:08:37 +0200

    refactoring

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -4,8 +4,8 @@ class DeploysController < ApplicationController
   def deploy
     @site = params[:site]
     @res = case @site
-    when "quartierlibreromont" then Open3.capture2e("bash lib/scripts/deploy_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
-    when "romont"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
+    when "quartierlibreromont.ch" then Open3.capture2e("bash lib/scripts/deploy_quartierlibreromont.sh #{Rails.application.secrets.fribourg_user} #{Rails.application.secrets.jsport}")
+    when "romont.eebulle.ch"   then Open3.capture2e("bash lib/scripts/deploy_romont.sh #{Rails.application.secrets.romont_user} #{Rails.application.secrets.jsport}")
       else ["Unknown site", -1]
     end
     @exit_code = @res[1].to_s[-1].to_i
