c636f8efb83621b950f16868475e7d5f60bca80f
Noémien Kocher <nkcr.je@gmail.com>
2018-04-23 11:47:27 +0200

    fix deploy script

--- lib/scripts/preview_romont.sh
+++ lib/scripts/preview_romont.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u spha ssh $1@146.185.163.64 -p $2 -o ConnectTimeout=10 -o PasswordAuthentication=false -o BatchMode=yes "cd /var/www/preview.romont.eebulle.ch/public_html/ && git pull && echo $'User-agent: *\nDisallow: /' > robot.txt"
+sudo -u spha ssh $1@146.185.163.64 -p $2 -o ConnectTimeout=10 -o PasswordAuthentication=false -o BatchMode=yes "cd /var/www/preview.romont.eebulle.ch/public_html/eebulle.romont && git pull && echo $'User-agent: *\nDisallow: /' > robot.txt"
